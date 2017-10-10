525629a0eb02d9a0a30e92d28d7ffaa1372db131
terrysky18 <terrysky83@gmail.com>
2017-10-10 17:23:42 -0400

    continuing Java lessons on Udemy

---
+++ BlueJTutorial/cars/Main.java
@@ -0,0 +1,12 @@
+ 
+
+public class Main {
+
+    public static void main(String[] args) {
+		Car porsche = new Car();
+		Car holden = new Car();
+
+		porsche.SetModel("Carrera GT");
+		System.out.println(porsche.GetModel());
+    }
+}
